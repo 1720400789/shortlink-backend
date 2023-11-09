a0b5f18c7781eef8c6e2a6c09e5ad1383fe59c22
zjluvcn <hello@zj.cn>
2023-11-09 20:31:52 +0800

    修改

---
+++ admin/src/main/java/org/zj/shortlink/admin/dao/entity/UserDO.java
@@ -0,0 +1,80 @@
+package org.zj.shortlink.admin.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 
+ * @TableName t_user
+ */
+@Data
+@TableName(value ="t_user")
+public class UserDO implements Serializable {
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * ID
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    @TableField(value = "username")
+    private String username;
+
+    /**
+     * 密码
+     */
+    @TableField(value = "password")
+    private String password;
+
+    /**
+     * 真实姓名
+     */
+    @TableField(value = "real_name")
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    @TableField(value = "phone")
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    @TableField(value = "mail")
+    private String mail;
+
+    /**
+     * 注销时间戳
+     */
+    @TableField(value = "deletion_time")
+    private Long deletionTime;
+
+    /**
+     * 创建时间
+     */
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableField(value = "del_flag", fill = FieldFill.INSERT)
+    private Integer delFlag;
+
+}
