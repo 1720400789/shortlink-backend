8862cf7163e98d620b9f835278373fe54f934b01
zjluvcn <hello@zj.cn>
2023-11-09 16:09:07 +0800

    真实用户返回类

---
+++ admin/src/main/java/org/zj/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -0,0 +1,39 @@
+package org.zj.shortlink.admin.dto.resp;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+import org.zj.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
+
+/**
+ * 未脱敏信息
+ */
+@Data
+public class UserActualRespDTO {
+    /**
+     * ID
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+}
