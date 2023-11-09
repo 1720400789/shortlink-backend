ed4040e81b4c1b3f6b04de15f41583380cf843d2
zjluvcn <hello@zj.cn>
2023-11-09 20:28:59 +0800

    利用MP实现元数据自动填充

---
+++ admin/src/main/java/org/zj/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,42 @@
+package org.zj.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Slf4j
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("公共字段自动填充[insert]...");
+        log.info(metaObject.toString());
+        if (metaObject.hasSetter("createTime")){
+            this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        }
+        if (metaObject.hasSetter("updateTime")) {
+            this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        }
+        if (metaObject.hasSetter("delFlag")) {
+            this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+        }
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        Long id = Thread.currentThread().getId();
+        log.info("线程id为：{}", id);
+
+        log.info("公共字段自动填充[update]...");
+        log.info(metaObject.toString());
+        if (metaObject.hasSetter("updateTime")) {
+            this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+        }
+    }
+}
