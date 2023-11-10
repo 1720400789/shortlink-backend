89fca757b47c2dc28fa08362592c2c23c732b1d1
zjluvcn <hello@zj.cn>
2023-11-10 16:14:39 +0800

    后管完善

---
+++ admin/src/main/java/org/zj/shortlink/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package org.zj.shortlink.admin.dao.mapper;
+
+import org.zj.shortlink.admin.dao.entity.UserDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 1720400789
+* @description 针对表【t_user】的数据库操作Mapper
+* @createDate 2023-11-08 19:59:39
+* @Entity generator.domain.TUser
+*/
+public interface UserMapper extends BaseMapper<UserDO> {
+
+}
+
+
+
+
