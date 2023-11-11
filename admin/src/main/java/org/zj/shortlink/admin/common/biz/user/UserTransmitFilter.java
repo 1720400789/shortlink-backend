df031a2ef0b221b3dbe62716274d8fba9b9fa350
zjluvcn <hello@zj.cn>
2023-11-11 21:33:51 +0800

    feature: 用户登录验证拦截器返回友好提示信息

--- admin/src/main/java/org/zj/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/zj/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,5 +1,6 @@
 package org.zj.shortlink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.google.common.collect.Lists;
 import jakarta.servlet.Filter;
@@ -8,10 +9,19 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.zj.shortlink.admin.common.convention.exception.ClientException;
+import org.zj.shortlink.admin.common.convention.result.Results;
+
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Objects;
+
+import static org.zj.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
  * 用户信息传输过滤器
@@ -21,19 +31,45 @@ public class UserTransmitFilter implements Filter {
 
     private final StringRedisTemplate stringRedisTemplate;
 
+    /**
+     * 不用过滤的路径
+     */
     private static final List<String> IGNORE_URI = Lists.newArrayList(
-            "/api/short-link/admin/v1/user/login"
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/v1/user/has-username"
     );
 
+    @SneakyThrows
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
+        // 如果不用过滤就直接跳过
         if (!IGNORE_URI.contains(requestURI)) {
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-            if (userInfoJsonStr != null) {
+            String method = httpServletRequest.getMethod();
+            // 因为 Restful 风格的书写，我们本来要排除注册接口的，但是这里是有 POST Method 才是注册的，所以这里再过滤一层
+            if (!(Objects.equals(requestURI, "/api/short-link/admin/v1/user") && Objects.equals(method, "POST"))) {
+                // 拿到请求头中的 username 和 token
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                // 如果 Token 不存在就抛出客户端异常给全局异常捕获
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                    return;
+                }
+                Object userInfoJsonStr = null;
+                try {
+                    // 检查 Token 是否存在
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    if (userInfoJsonStr == null) {
+                        returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                        return;
+                    }
+                } catch (Exception ex) {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                    return;
+                }
+                // 根据 redis 中 token 存储的 user 信息，将 user 信息设置在 User 上下文中
                 UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
             }
@@ -43,5 +79,21 @@ public class UserTransmitFilter implements Filter {
         } finally {
             UserContext.removeUser();
         }
+    }
+
+    private void returnJson(HttpServletResponse response, String json) throws Exception {
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            writer.print(json);
+        } catch (IOException e) {
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+
     }
 }
