fab80189302fbccd750ea45cd1117738ee42d60d
zjluvcn <hello@zj.cn>
2023-12-24 17:44:21 +0800

    feature: 开发短链接监控之分组短链接访问记录

--- admin/src/main/java/org/zj/shortlink/admin/controller/ShortLinkStatsController.java
+++ admin/src/main/java/org/zj/shortlink/admin/controller/ShortLinkStatsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.zj.shortlink.admin.common.convention.result.Result;
 import org.zj.shortlink.admin.common.convention.result.Results;
 import org.zj.shortlink.admin.remote.ShortLinkRemoteService;
+import org.zj.shortlink.admin.remote.dto.req.ShortLinkGroupStatsAccessRecordReqDTO;
 import org.zj.shortlink.admin.remote.dto.req.ShortLinkGroupStatsReqDTO;
 import org.zj.shortlink.admin.remote.dto.req.ShortLinkStatsAccessRecordReqDTO;
 import org.zj.shortlink.admin.remote.dto.req.ShortLinkStatsReqDTO;
@@ -50,5 +51,13 @@ public class ShortLinkStatsController {
     @GetMapping("/api/short-link/admin/v1/stats/group")
     public Result<ShortLinkStatsRespDTO> groupShortLinkStats(ShortLinkGroupStatsReqDTO requestParam) {
         return shortLinkRemoteService.groupShortLinkStats(requestParam);
+    }
+
+    /**
+     * 访问分组短链接指定时间内访问记录监控数据
+     */
+    @GetMapping("/api/short-link/admin/v1/stats/access-record/group")
+    public Result<IPage<ShortLinkStatsAccessRecordRespDTO>> groupShortLinkStatsAccessRecord(ShortLinkGroupStatsAccessRecordReqDTO requestParam) {
+        return shortLinkRemoteService.groupShortLinkStatsAccessRecord(requestParam);
     }
 }
