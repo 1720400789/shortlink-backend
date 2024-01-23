65ae4fabbcb0672346a842f94a5f52af9686ccce
zjluvcn <hello@zj.cn>
2024-01-23 22:22:26 +0800

    optimize: 修复

--- project/src/main/java/org/zj/shortlink/project/mq/producer/ShortLinkStatsSaveProducer.java
+++ project/src/main/java/org/zj/shortlink/project/mq/producer/ShortLinkStatsSaveProducer.java
@@ -19,8 +19,8 @@ public class ShortLinkStatsSaveProducer {
 
     private final StringRedisTemplate stringRedisTemplate;
 
-    @Value("${spring.data.redis.channel-topic.short-link-stats}")
-    private String topic;
+//    @Value("${spring.data.redis.channel-topic.short-link-stats}")
+//    private String topic;
 
     /**
      * 发送延迟消费短链接统计
